2e759687285303ed966dfb91058a5ac8c8e48962
Charlote <cadzialo@gmail.com>
2019-05-16 16:39:06 -0700

    update answers

--- sql_zoo/join/join11.sql
+++ sql_zoo/join/join11.sql
@@ -1,7 +1,8 @@
 
+
 SELECT matchid, mdate, COUNT(matchid)
 FROM game
 JOIN goal
 ON game.id = goal.matchid
-WHERE team1 = 'POL'  OR team2 = 'POL'  
+WHERE team1 = 'POL' OR team2 = 'POL'  
 GROUP BY matchid,mdate; 
