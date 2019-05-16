a630770e9b7f7f26f4e0b978548acc3422afe55f
Charlote <cadzialo@gmail.com>
2019-05-16 14:47:24 -0700

    more sql

---
+++ sql_zoo/select_within_select/sws5.sql
@@ -0,0 +1,17 @@
+'''
+Germany (population 80 million) has the largest population 
+of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
+
+Show the name and the population of each country in Europe. 
+Show the population as a percentage of the population of Germany.
+'''
+
+SELECT name, 
+    CONCAT(ROUND( 
+            100*(population/
+                    (SELECT population 
+                      FROM world 
+                     WHERE name = 'Germany')
+                ),0),'%') as percentage
+FROM world 
+WHERE continent = 'Europe'; 
---
+++ sql_zoo/select_within_select/sws6.sql
@@ -0,0 +1,7 @@
+SELECT name 
+ FROM world
+WHERE gdp > 
+    ALL(SELECT gdp 
+            FROM world 
+            WHERE continent = 'Europe' 
+            AND gdp > 0); 
